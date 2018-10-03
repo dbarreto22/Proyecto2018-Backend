e71adf0f2867bbca3a6903d0a9c9fbe5d32e157b
Ernesto <ERPEFE2006@gmail.com>
2018-10-03 05:46:56 -0300

    Agrego rest con carreras.

--- Server/src/main/java/com/miudelar/server/logic/managers/RestExample.java
+++ Server/src/main/java/com/miudelar/server/logic/managers/RestExample.java
@@ -7,9 +7,10 @@ package com.miudelar.server.logic.managers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
  
-@Path("/user")
+@Path("/main")
 public class RestExample
 {
     @GET
@@ -17,6 +18,15 @@ public class RestExample
     public Response getAllUsers()
     {
         String result = "<h1>RESTful Demo Application</h1>In real world application, a collection of users will be returned !!";
+        return Response.status(200).entity(result).build();
+    }
+    
+    @GET
+    @Path("/carreras")
+    @Produces("application/json")
+    public Response getAllCarreras()
+    {
+        String result = "{ \"carreras\": [ { \"id\": 0, \"name\": \"Ingenieria Electrica\" }, { \"id\": 1, \"name\": \"Licenciatura en Psicologia\" }, { \"id\": 2, \"name\": \"Tecnologo en Informatica\" } ] }";
         return Response.status(200).entity(result).build();
     }
 }
