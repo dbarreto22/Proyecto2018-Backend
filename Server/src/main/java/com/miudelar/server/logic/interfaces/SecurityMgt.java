d317ee2f17765e56511cd261c10498614f1fad35
romina moreno <rmoreno@gxcl614.Artech.local>
2018-10-06 15:46:05 -0300

    gitignore

--- Server/src/main/java/com/miudelar/server/logic/interfaces/SecurityMgt.java
+++ Server/src/main/java/com/miudelar/server/logic/interfaces/SecurityMgt.java
@@ -6,9 +6,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 
 
 /* @author Romina*/
+
 @Path("/security")
 public interface SecurityMgt {
     @GET
